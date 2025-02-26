fe08759b61e356858cb3270d0c12e79bf7c72b81
alagar2604 <alagarsamy200401@gmail.com>
2025-02-26 13:10:30 +0530

    my webpage

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary ">
   <div className="container-fluid ">
-   
+  <img src="./assets/logo.jpg" alt="" />
    <h1 className='fw-bold fs-2 text-light'>Alagarsamy</h1>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
