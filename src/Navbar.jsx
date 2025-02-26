4476b34ff160619192f4a8ec47e38b446e8a58a6
Alagar <alagarsamy200401@gmail.com>
2025-02-26 13:06:10 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary ">
   <div className="container-fluid ">
-  <i class="fa-brands fa-algolia fs-2 fw-bold mx-2 text-light"></i>
+   <img src="./assets/logo.jpg" alt="" />
    <h1 className='fw-bold fs-2 text-light'>Alagarsamy</h1>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
