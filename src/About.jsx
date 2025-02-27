a7b00c9351252d6c8775a6c2425a846997876096
alagar2604 <alagarsamy200401@gmail.com>
2025-02-27 11:23:00 +0530

    my webpage

--- src/About.jsx
+++ src/About.jsx
@@ -40,7 +40,7 @@ function About() {
             <div className=" col-12 col-lg-6  ">
                <div className="container h-100 ">
                  
-                 <img src="./assets/logo.jpg" className='w-100 border border-1 border-light p-3 ' alt="" height={"100%"}  />
+                 <img src="https://images.pexels.com/photos/5935788/pexels-photo-5935788.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className='w-100 border border-1 border-light p-3 ' alt="" height={"100%"}  />
                  
                </div>
                
